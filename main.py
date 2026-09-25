e586b015782b07b242fc6da3d16ceaf47b93aaea
Dr-Seamus-Cummins <seamus.cummins@dcu.ie>
2026-09-25 10:49:14 +0100

    Remove turtle graphics code and print 'Hello'

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import turtle
-
-pen = turtle.Turtle()
-pen.color("blue")
-pen.pensize(4)
-pen.speed(3)
-
-for side in range(4):
-    pen.forward(100)
-    pen.left(90)
-
-pen.hideturtle()
+print ("Hello")
